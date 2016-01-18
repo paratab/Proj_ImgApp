2f4e0115a0c65a5cadfc14c1508ef7b8002dfa8c
Surawit Sangjan <iamdreammy@hotmail.com>
2016-01-18 21:52:45 +0700

    Implement Notice Class

--- app/src/main/java/bolona_pig/proj_imgapp/Notice.java
+++ app/src/main/java/bolona_pig/proj_imgapp/Notice.java
@@ -6,9 +6,20 @@ package bolona_pig.proj_imgapp;
 public class Notice {
 
     int noticeId;
+    String lp_name, lp_birthdate, lp_location, lp_date, lp_detail, ct_name, ct_phone;
 
-    public Notice() {
+    public Notice(String lp_name, String lp_birthdate, String lp_date, String lp_location, String lp_detail, String ct_name, String ct_phone) {
+        this.lp_name = lp_name;
+        this.lp_birthdate = lp_birthdate;
+        this.lp_date = lp_date;
+        this.lp_location = lp_location;
+        this.lp_detail = lp_detail;
+        this.ct_name = ct_name;
+        this.ct_phone = ct_phone;
+    }
 
+    public void setNoticeId(int id) {
+        this.noticeId = id;
     }
 
 }
