f82936f0b66cd4562b27a047f4fc841f6de73283
Surawit Sangjan <iamdreammy@hotmail.com>
2016-02-15 10:40:24 +0700

    14/2/2559 - fix wrong display because of recycle grid of gridview

--- app/src/main/java/bolona_pig/proj_imgapp/ObjectClass/NoticeGridAdapter.java
+++ app/src/main/java/bolona_pig/proj_imgapp/ObjectClass/NoticeGridAdapter.java
@@ -1,7 +1,6 @@
 package bolona_pig.proj_imgapp.ObjectClass;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -53,24 +52,23 @@ public class NoticeGridAdapter extends BaseAdapter {
         View gridView;
 
         if (convertView == null) {
-
             gridView = inflater.inflate(R.layout.grid_item_layout, null);
-
-            TextView textView1 = (TextView) gridView.findViewById(R.id.grid_title1);
-            TextView textView2 = (TextView) gridView.findViewById(R.id.grid_title2);
-            ImageView imageView = (ImageView) gridView.findViewById(R.id.grid_item_image);
-
-            GridItem item = mGridData.get(position);
-            Log.d("custom_check", "position : " + position + " Name :" + item.title1 + " BirthDate : " + item.title2);
-
-            String temp = "ชื่อ : " + item.title1;
-            textView1.setText(temp);
-            temp = "อายุ : " + dateTime.getAge(item.title2) + " ปี";
-            textView2.setText(temp);
-            Picasso.with(mContext).load(item.image).into(imageView);
         } else {
             gridView = convertView;
         }
+
+        TextView textView1 = (TextView) gridView.findViewById(R.id.grid_title1);
+        TextView textView2 = (TextView) gridView.findViewById(R.id.grid_title2);
+        ImageView imageView = (ImageView) gridView.findViewById(R.id.grid_item_image);
+
+        GridItem item = mGridData.get(position);
+        //Log.d("custom_check", "position : " + position + " Name :" + item.title1 + " BirthDate : " + item.title2);
+
+        String temp = "ชื่อ : " + item.title1;
+        textView1.setText(temp);
+        temp = "อายุ : " + dateTime.getAge(item.title2) + " ปี";
+        textView2.setText(temp);
+        Picasso.with(mContext).load(item.image).into(imageView);
 
         return gridView;
     }
